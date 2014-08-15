9094ed209687938407898e8241b0779b4d403490
Justin Beuck <jbeuck@yahoo.com>
2014-08-15 14:52:52 -0500

    added random bar and restaurant logic

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'pry-byebug'
 set :bind, '0.0.0.0'
 use Rack::Flash
 
-get "/" do
+get "/randomrestaurant" do
   # binding.pry
 
   data = {
@@ -27,12 +27,24 @@ get "/" do
   token = "udKMZsaQQ5B3UuXNob4jHtOuzPW4scNl"
   token_secret = "qXl5GqbmqxR-Mk6J9QuCen-84Bc"
 
-  path = "/v2/search?term=restaurants+#{params['Restaurant']}&location=austin"
+  styles = [
+    "sushi", "thai", "mexican", "pizza", "burgers",
+    "hotdogs", "american", "deli", "italian", "chinese",
+    "french", "bar", "irish", "vegetarian", "vegan",
+    "sandwiches", "tacos", "beer", "texmex", "chinese", 
+    "barbeque", "seafood", "steakhouses", "asianfusion"
+  ]
+
+  random_choice = styles.shuffle.first
+  random_path = "/v2/search?term=bar+#{random_choice}&location=austin"
+
+  # user_path = "/v2/search?term=restaurants+#{params['Restaurant']}&location=austin"
 
   consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://api.yelp.com"})
   access_token = OAuth::AccessToken.new(consumer, token, token_secret)
 
   @search_response = JSON(access_token.get(path).body)
+  @restaurant = @search_response["businesses"].shuffle.first
 
   # @restaurant = search_response['businesses'].first['name']
   # @url = search_response['businesses'].first['url']
@@ -50,7 +62,58 @@ get "/" do
   erb :index
 end
 
+get "/randombar" do
+  # binding.pry
 
-get "/" do
+  data = {
+    term: params["bar"],
+    limit: 3
+  }
 
+  p data
+
+  # client = Yelp::Client.new({ consumer_key: "q_5VHCkxQcT1B4hxcCM_2w",
+  #                             consumer_secret: "4HOFnWO9NT0anZexVqmiVyKzp5Q",
+  #                             token: "udKMZsaQQ5B3UuXNob4jHtOuzPW4scNl",
+  #                             token_secret: "qXl5GqbmqxR-Mk6J9QuCen-84Bc"
+  #                           })
+
+  consumer_key = "q_5VHCkxQcT1B4hxcCM_2w"
+  consumer_secret = "4HOFnWO9NT0anZexVqmiVyKzp5Q"
+  token = "udKMZsaQQ5B3UuXNob4jHtOuzPW4scNl"
+  token_secret = "qXl5GqbmqxR-Mk6J9QuCen-84Bc"
+
+  styles = [
+    "sushi", "thai", "mexican", "pizza", "burgers",
+    "hotdogs", "american", "deli", "italian", "chinese",
+    "french", "bar", "irish", "vegetarian", "vegan",
+    "sandwiches", "tacos", "beer", "texmex", "chinese", 
+    "barbeque", "seafood", "steakhouses", "asianfusion"
+  ]
+
+  random_choice = styles.shuffle.first
+  random_path = "/v2/search?term=bar+#{random_choice}&location=austin"
+
+  # user_path = "/v2/search?term=bar+#{params['Restaurant']}&location=austin"
+
+  consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://api.yelp.com"})
+  access_token = OAuth::AccessToken.new(consumer, token, token_secret)
+
+  @search_response = JSON(access_token.get(path).body)
+  @restaurant = @search_response["businesses"].shuffle.first
+
+  # @restaurant = search_response['businesses'].first['name']
+  # @url = search_response['businesses'].first['url']
+
+  # "<a href='#{search_response['businesses'].first['url']}'>#{search_response['businesses'].first['name']}</a>"
+
+  # response = client.search('Austin', data)#, { lang: 'en' })
+  # search_results = JSON.parse(response.to_json)
+  # first_name = search_results["businesses"].first["name"]
+  # first_url = search_results["businesses"].first["url"]
+
+  # @restaurant = first_name
+  # @url = first_url
+
+  erb :index
 end
